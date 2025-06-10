f5f3b6a978d75ac4043c287a0c51736dce21a895
lh <35221199@qq.com>
2025-06-10 10:06:06 +0800

    fix

--- admin/src/views/devices/components/columns.tsx
+++ admin/src/views/devices/components/columns.tsx
@@ -1,7 +1,6 @@
 import { ColumnDef } from '@tanstack/react-table'
-import { Badge } from '@/components/ui/badge'
 import { Checkbox } from '@/components/ui/checkbox'
-import { labels, statuses } from '../data/data'
+import {  statuses } from '../data/data'
 import { DeviceModel } from '../data/schema'
 import { DataTableColumnHeader } from './data-table-column-header'
 import { DataTableRowActions } from './data-table-row-actions'
